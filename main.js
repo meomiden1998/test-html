7cc77f3a7af9134ff9f7d0700398231d6a699195
aly <meomeow1998nd@gmail.com>
2022-12-17 00:43:40 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function openMenu(){
+    var a= document.getElementById("nav1") ;
+    a.style.display="block"
+}
+
+function closeMenu(){
+    var a= document.getElementById("nav1") ;
+    a.style.display="none"
+}
+
+function slide(){
+
+}
+
+
